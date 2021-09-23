16e00a684a0eb6511aaa1860efc235e2751385d5
Zvonimir Perkovic <zperkovi@gmail.com>
2021-09-23 16:10:07 -0400

    First test.

---
+++ script.test.js
@@ -0,0 +1,17 @@
+const googleSearch = require('./script');
+
+dbMock = [
+  'dog.com',
+  'cheese.com',
+  'dogpictures.com'
+];
+
+it('this is a test', () => {
+  expect('hello').toBe('hello');
+  googleSearch('testtest', dbMock);
+})
+
+it('is searching google', () => {
+  expect(googleSearch('testtest', dbMock)).toEqual([]);
+  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+})
